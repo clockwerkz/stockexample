6ee18d1812837ff5208c4dd31a7bed948140795d
Carlos <clockwerkz@gmail.com>
2021-10-28 07:33:05 -0700

    adds stock fetching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { apiKey } from "../src/config/keys";
 function App() {
+  const [stockList, editStockList] = useState(["AAPL", "MSFT", "GOOG"]);
+  const [stockData, editStockData] = useState([]);
+
+  useEffect(() => {
+    Promise.all(
+      stockList.map((stock) =>
+        fetch(
+          `https://financialmodelingprep.com/api/v3/quote/${stock}?apikey=${apiKey}`
+        )
+      )
+    ).then((results) =>
+      Promise.all(results.map((res) => res.json())).then((stocks) => {
+        editStockData(stocks);
+      })
+    );
+  }, [stockList]);
+
   return (
     <div className="App">
       <h1>Stock App</h1>
